87d0f056631748462936781dc1038a5dff99e311
vtemian <vladtemian@gmail.com>
2011-10-08 17:23:06 +0300

    db

--- settings.py
+++ settings.py
@@ -14,10 +14,10 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': PROJECT_ROOT + '/geekmeet.sql',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'geekmeet',                      # Or path to database file if using sqlite3.
+        'USER': 'geekmeet',                      # Not used with sqlite3.
+        'PASSWORD': 'geekmeet',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
